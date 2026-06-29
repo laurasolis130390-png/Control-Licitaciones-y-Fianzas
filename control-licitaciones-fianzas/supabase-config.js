e6a7e743907854bd9564172b0fd23c94d1289534
laurasolis130390-png <laura.solis130390@gmail.com>
2026-06-29 12:21:44 -0600

    Add files via upload

---
+++ control-licitaciones-fianzas/supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://icgmlndxhuajzgvgfgse.supabase.co/rest/v1/",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljZ21sbmR4aHVhanpndmdmZ3NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3NDgzMTAsImV4cCI6MjA5ODMyNDMxMH0.fsvyJpD51blKsR9bc9kWniPTFwXPKGBeCREniO_u-9k",
+};
